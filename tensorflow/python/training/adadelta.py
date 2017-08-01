36dd9470581de156a56744a09eb7b74759f743eb
changgongcheng <mjcheng@mail.ustc.edu.cn>
2017-08-01 16:37:29 +0800

    Update adadelta.py

--- tensorflow/python/training/adadelta.py
+++ tensorflow/python/training/adadelta.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-###chang gong chengdd
+###chang gong cheng
 """Adadelta for TensorFlow."""
 from __future__ import absolute_import
 from __future__ import division
